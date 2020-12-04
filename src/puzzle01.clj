3668ac8e0eb955819e0a554352ffe902f8dd7315
Pranav Bhaskar <pranavbhaskar17@gmail.com>
2020-12-04 23:36:19 +0530

    first commit

---
+++ src/puzzle01.clj
@@ -0,0 +1,29 @@
+(ns puzzle01
+  (:require [clojure.java.io :as io]))
+
+(def demo-input [1721
+            979
+            366
+            299
+            675
+            1456])
+
+(def input
+  (map #(Long/parseLong %)
+     (line-seq (io/reader (io/resource "puzzle01_input.txt")))))
+
+(first
+ (for [x input
+      y input
+      :when (= 2020 (+ x y))]
+  (* x y)))
+;; => 73371
+
+; Part two
+(first
+ (for [x input
+       y input
+       z input
+      :when (= 2020 (+ x y z))]
+  (* x y z)))
+;; => 127642310
---
+++ src/puzzle03.clj
@@ -0,0 +1,60 @@
+(ns puzzle03
+  (:require [clojure.string :as str])
+  (:require [clojure.java.io :as io]))
+
+(def demo-input "..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#")
+
+(def parse-demo (str/split demo-input #"\n"))
+
+(defn tree? [v x y]
+  (= \# (nth (nth v y) (mod x (count (first v))))))
+
+(defn path [v n1 n2]
+  (loop [x n1 y n2 p []]
+    (if (< y (dec (count v)))
+      (recur (+ x n1) (+ y n2) (conj p (vector x y)))
+      (conj p (vector x y)))))
+
+(count (filter
+        #(let [[x y] %]
+           (tree? parse-demo x y))
+ (path parse-demo 3 1)))
+;; => 7
+
+(def input (line-seq (io/reader (io/resource "puzzle03_input.txt"))))
+
+(count (filter
+        #(let [[x y] %]
+           (tree? input x y))
+ (path input 3 1)))
+;; => 218
+
+;; Part 2
+(reduce
+ * (map
+    (fn [s]
+      (count
+       (filter #(let [[x y] %] (tree? parse-demo x y))
+        (path parse-demo (first s) (last s)))))
+    [[1 1] [3 1] [5 1] [7 1] [1 2]]))
+;; => 336
+
+(reduce
+ * (map
+    (fn [s]
+      (count
+       (filter #(let [[x y] %] (tree? input x y))
+        (path input (first s) (last s)))))
+    [[1 1] [3 1] [5 1] [7 1] [1 2]]))
+;; => 3847183340
+
